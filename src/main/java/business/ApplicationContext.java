74bb8551dbc0a747807eac1889efab982d2dc5f2
John Smith <54953396+yuqinbs@users.noreply.github.com>
2021-10-13 19:40:19 -0400

    Project 4 Part A completed

---
+++ src/main/java/business/ApplicationContext.java
@@ -0,0 +1,29 @@
+
+package business;
+
+import business.book.BookDao;
+import business.book.BookDaoJdbc;
+import business.category.CategoryDao;
+import business.category.CategoryDaoJdbc;
+
+public class ApplicationContext {
+
+
+    private BookDao bookDao;
+
+    private CategoryDao categoryDao;
+
+    public static ApplicationContext INSTANCE = new ApplicationContext();
+
+    private ApplicationContext() {
+        bookDao = new BookDaoJdbc();
+        //jdbc java database controller
+        categoryDao = new CategoryDaoJdbc();
+    }
+
+    public CategoryDao getCategoryDao() {
+        return categoryDao;
+    }
+
+    public BookDao getBookDao() { return bookDao; }
+}
